641cc1ec2c84c5cdd90c36a15ab58709895b9046
wael-morjen <waelmorjen@gmail.com>
2023-07-19 00:32:26 +0100

    Settings page UI

---
+++ admin/app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+
+
+interface SettingsFormProps {
+    initialData: Store;
+}
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({
+    initialData
+}) => {
+    return (
+        <>
+            <div className="flex items-center justify-between">
+                <Heading
+                    title="Settings"
+                    description="Manage your store"
+                />
+                <Button
+                    variant="destructive"
+                    size="icon"
+                    onClick={() => {}}
+                >
+                    <Trash className="h-4 w-4"/>
+                </Button>
+            </div>
+            
+        </>
+    )
+}
--- admin/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,7 +1,39 @@
-const SettingPage = () => {
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { SettingsForm } from "./components/SettingsForm";
+
+interface SettingsPageProps {
+    params: {
+        storeId: string;
+    }
+}
+
+const SettingPage: React.FC<SettingsPageProps> = async ({
+    params
+}) => {
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect("/sign-in")
+    }
+
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId
+        }
+    })
+
+    if (!store) {
+        redirect("/")
+    }
+
     return (
-        <div>
-            Settings
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <SettingsForm initialData={store}/>
+            </div>
         </div>
     );
 }
