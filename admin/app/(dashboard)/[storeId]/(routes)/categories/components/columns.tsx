eb566bf7fb09d5a3a97711819fb159c3549414e7
wael-morjen <waelmorjen@gmail.com>
2023-07-21 19:39:35 +0100

    Categories layout

---
+++ admin/app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { CellAction } from "./cell-actions"
+
+
+export type CategoryColumn = {
+  id: string
+  name: string
+  billboardLabel: string
+  createdAt: string
+}
+
+export const columns: ColumnDef<CategoryColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "billboard",
+    header: "Billboard",
+    cell: ({ row }) => row.original.billboardLabel,
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+    cell: ({ row }) => row.original.billboardLabel,
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />
+  }
+]
