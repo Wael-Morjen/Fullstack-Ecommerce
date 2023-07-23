9353961373b87c0ea5484d44b8f0435a90a89aa6
wael-morjen <waelmorjen@gmail.com>
2023-07-23 16:28:14 +0100

    Sizes components layout

--- admin/components/MainNav.tsx
+++ admin/components/MainNav.tsx
@@ -30,6 +30,11 @@ export function MainNav ({
             active: pathName === `/${params.storeId}/categories`
         },
         {
+            href: `/${params.storeId}/sizes`,
+            label: "Sizes",
+            active: pathName === `/${params.storeId}/sizes`
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: "Settings",
             active: pathName === `/${params.storeId}/settings`
