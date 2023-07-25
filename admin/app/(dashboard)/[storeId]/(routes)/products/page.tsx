2f1fdfe90c820c1369e4bf29a0c58f0dd2de1cd2
wael-morjen <waelmorjen@gmail.com>
2023-07-25 19:52:07 +0100

    Products routes & API

--- admin/app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -31,7 +31,7 @@ const PruductsPage = async ({
         isArchived: item.isArchived,
         price: formatter.format(item.price.toNumber()),
         category: item.category.name,
-        size: item.category.name,
+        size: item.size.name,
         color: item.color.value,
         createdAt: format(item.createdAt, "MMMM do, yyyy")
     }))
