16fae7c77c3b5497fc2d1fce13aeef5252d3b69e
wael-morjen <waelmorjen@gmail.com>
2023-07-25 23:14:11 +0100

    Integrate Orders in MainNav

--- admin/components/MainNav.tsx
+++ admin/components/MainNav.tsx
@@ -45,6 +45,11 @@ export function MainNav ({
             active: pathName === `/${params.storeId}/products`
         },
         {
+            href: `/${params.storeId}/orders`,
+            label: "Orders",
+            active: pathName === `/${params.storeId}/orders`
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: "Settings",
             active: pathName === `/${params.storeId}/settings`
