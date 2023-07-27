7ce3cbc9f6652099678aafa0adbaad1d290fa1c6
wael-morjen <waelmorjen@gmail.com>
2023-07-27 22:24:18 +0100

    Navbar component

--- store/components/NavbarActions.tsx
+++ store/components/NavbarActions.tsx
@@ -1,10 +1,21 @@
 'use client';
 
+import { useEffect, useState } from "react";
 import Button from "./ui/button";
 import { ShoppingBag } from "lucide-react";
 
 
 const NavbarActions = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, [])
+
+    if (!isMounted) {
+        return null;
+    }
+    
     return (
         <div className="ml-auto flex items-center gap-x-4">
             <Button className="flex items-center rounded-full bg-black px-4 py-2">
