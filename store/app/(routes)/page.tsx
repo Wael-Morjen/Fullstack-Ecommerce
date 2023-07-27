1d362e96bd878cc636d5e524c6f4c610876524d0
wael-morjen <waelmorjen@gmail.com>
2023-07-27 22:48:14 +0100

    get billboards

--- store/app/(routes)/page.tsx
+++ store/app/(routes)/page.tsx
@@ -1,6 +1,17 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboards";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard("39dbda96-112b-4946-ae4e-23bdf1447ae8")
     return (
-        <div>Hello Store</div>
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard}/>
+            </div>
+        </Container>
     );
 }
  
