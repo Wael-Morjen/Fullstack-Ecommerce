9f0369b608e95aa2e1507de3659c4322891834ef
wael-morjen <waelmorjen@gmail.com>
2023-07-27 23:33:12 +0100

    get products

--- store/app/(routes)/page.tsx
+++ store/app/(routes)/page.tsx
@@ -1,15 +1,21 @@
 import getBillboard from "@/actions/get-billboards";
+import getproducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+    const products = await getproducts({ isFeatured: true})
     const billboard = await getBillboard("39dbda96-112b-4946-ae4e-23bdf1447ae8")
     return (
         <Container>
             <div className="space-y-10 pb-10">
                 <Billboard data={billboard}/>
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products} />
             </div>
         </Container>
     );
---
+++ store/components/product-list.tsx
@@ -0,0 +1,35 @@
+import { Product } from "@/types";
+import NoResults from "./ui/no-results";
+
+interface ProductListProps {
+    title: string;
+    items: Product[];
+};
+
+const ProductList: React.FC<ProductListProps> = ({
+    title,
+    items
+}) => {
+    return (
+        <div className="space-y-4">
+            <h3 className="font-bold text-3xl">{title}</h3>
+            {items.length === 0 && <NoResults />}
+            <div className="
+                grid 
+                grid-cols-1
+                sm:grid-cols-2
+                md:grid-cols-3
+                lg:grid-cols-4
+                gap-4
+            ">
+                {items.map((item) => (
+                    <div key={item.id}>
+                        {item.name}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+ 
+export default ProductList;
