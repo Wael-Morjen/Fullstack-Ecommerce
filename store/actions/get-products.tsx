842f4b00fc8fe4edbdbf7711c9d6201160c9c091
wael-morjen <waelmorjen@gmail.com>
2023-07-29 01:50:16 +0100

    related products

--- store/actions/get-products.tsx
+++ store/actions/get-products.tsx
@@ -1,30 +1,29 @@
 import { Product } from "@/types";
-
 import qs from "query-string";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
+const URL=`${process.env.NEXT_PUBLIC_API_URL}/products`;
 
 interface Query {
-    categoryId?: string;
-    colorId?: string;
-    sizeId?: string;
-    isFeatured?: boolean;
+  categoryId?: string;
+  colorId?: string;
+  sizeId?: string;
+  isFeatured?: boolean;
 }
 
-const getproducts = async (query: Query): Promise<Product[]> => {
-    const url = qs.stringifyUrl({
-        url: URL,
-        query: {
-            colorId: query.colorId,
-            sizeId: query.sizeId,
-            categoryId: query.categoryId,
-            isFeatured: query.isFeatured,
-        },
-    });
-    
-    const res = await fetch(URL)
+const getProducts = async (query: Query): Promise<Product[]> => {
+  const url = qs.stringifyUrl({
+    url: URL,
+    query: { 
+      colorId: query.colorId,
+      sizeId: query.sizeId,
+      categoryId: query.categoryId,
+      isFeatured: query.isFeatured,
+    },
+  });
 
-    return res.json()
-}
+  const res = await fetch(url);
 
-export default getproducts;
+  return res.json();
+};
+
+export default getProducts;
--- store/components/ui/product-card.tsx
+++ store/components/ui/product-card.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
+import { useRouter } from "next/navigation";
 
 
 interface ProductCardProps {
@@ -14,8 +15,13 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({
     data
 }) => {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push(`/product/${data?.id}`)
+    }
     return (
-        <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+        <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
             {/* Images & Actions */}
             <div className="aspect-square rounded-xl bg-gray-100 relative">
                 <Image 
