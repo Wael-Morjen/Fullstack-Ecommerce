fbcab2f373d8117f230136be5e74f7e512b94f32
wael-morjen <waelmorjen@gmail.com>
2023-07-30 20:30:12 +0100

    Individual category screen & mobile filters

--- store/app/(routes)/category/[categoryId]/page.tsx
+++ store/app/(routes)/category/[categoryId]/page.tsx
@@ -7,6 +7,7 @@ import Container from "@/components/ui/container";
 import Filter from "./components/filter";
 import NoResults from "@/components/ui/no-results";
 import ProductCard from "@/components/ui/product-card";
+import MobileFilters from "./components/mobile-filters";
 
 export const revalidate = 0;
 
@@ -43,6 +44,10 @@ const CategorPage: React.FC<CategoryPageProps> = async ({
                 <div className="px-4 sm:px-6 lg:px-8 pb-24">
                     <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
                         {/* Add Mobile filters */}
+                        <MobileFilters 
+                            sizes={sizes}
+                            colors={colors}
+                        />
                         <div className="hidden lg:block">
                             <Filter
                                 valueKey="sizeId"
