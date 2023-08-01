ffaf67826e433c0b318a9b0f61f05edc2543c768
wael-morjen <waelmorjen@gmail.com>
2023-08-01 17:29:44 +0100

    summary & checkout funtionality

---
+++ store/app/(routes)/cart/components/summary.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { toast } from 'react-hot-toast';
+
+import Button from '@/components/ui/button';
+import Currency from '@/components/ui/currency';
+import useCart from '@/hooks/use-cart';
+
+
+const Summary = () => {
+    const searchParams = useSearchParams()
+    const items = useCart((state) => state.items);
+    const removeAll = useCart((state => state.removeAll))
+
+    const totalPrice = items.reduce((total, item) => {
+        return total + Number(item.price)
+    }, 0)
+
+    const onCheckout = async () => {
+        const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/chechout`, {
+            productIds: items.map((item) => item.id),
+        });
+
+        window.location = res.data.url
+    }
+
+    return (
+        <div
+            className='
+                bg-gray-50
+                mt-16
+                rounded-lg
+                px-4
+                py-6
+                sm:p-6
+                lg:col-span-5
+                lg:mt-0
+                lg:p-8
+            '
+        >
+            <h2 className='text-lg font-medium text-gray-900'>
+                Order Summary
+            </h2>
+            <div className='mt-6 space-y-4'>
+                <div className='flex items-center justify-between border-t border-gray-300 pt-4'>
+                    <div className='text-base font-medium text-gray-900'>
+                        Order Total
+                    </div>
+                    <Currency value={totalPrice}/>
+                </div>
+            </div>
+            <Button onClick={onCheckout} className='w-full mt-6'>
+                Checkout
+            </Button>
+        </div>
+    );
+}
+ 
+export default Summary;
--- store/app/(routes)/cart/page.tsx
+++ store/app/(routes)/cart/page.tsx
@@ -4,6 +4,7 @@ import Container from "@/components/ui/container";
 import useCart from "@/hooks/use-cart";
 import { useEffect, useState } from "react";
 import CartItem from "./components/cart-item";
+import Summary from "./components/summary";
 
 const CartPage = () => {
     const cart = useCart();
@@ -15,7 +16,7 @@ const CartPage = () => {
                     <h1 className="text-3xl font-bold text-black">
                         Shopping Cart
                     </h1>
-                    <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-w-12">
+                    <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
                         <div className="lg:col-span-7">
                             {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart</p>}
                             <ul>
@@ -27,6 +28,7 @@ const CartPage = () => {
                                 ))}
                             </ul>
                         </div>
+                        <Summary />
                     </div>
                 </div>
             </Container>   
